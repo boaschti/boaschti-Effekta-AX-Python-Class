7e9fb717c529a385e43d285224cb877f94943f88
Boaschti <sebastian-weisser@gmx.de>
2020-05-30 18:09:11 +0200

    Test der neuen Funktionen hinzugefügt

--- Akkuschutzfunktion_testbar.py
+++ Akkuschutzfunktion_testbar.py
@@ -19,6 +19,7 @@ VerbraucherAkku = "POP02"       # load prio 00=Netz, 02=Batt, 01=PV und Batt, we
 BattLeer = "PSDV43.0"
 BattWiederEntladen = "PBDV48.0"
 EntladeFreigabeGesendet = False
+NetzLadenAusGesperrt = False
 
 def schalteAlleWrAufAkku():
     BmsWerte["WrMode"] = VerbraucherAkku
@@ -28,6 +29,10 @@ def schalteAlleWrAufAkku():
 def schalteAlleWrNetzLadenAus():
     # Funktion ok, wr schaltet netzladen aus
     BmsWerte["WrNetzladen"] = False
+
+def schalteAlleWrNetzLadenEin():
+    # Funktion ok, wr schaltet netzladen aus
+    BmsWerte["WrNetzladen"] = True
 
 def schalteAlleWrVerbraucherPVundNetz():
     BmsWerte["WrMode"] = VerbraucherPVundNetz
@@ -43,11 +48,13 @@ def schalteAlleWrAufNetz():
     BmsWerte["WrEntladeFreigabe"] = False
     BmsWerte["WrNetzladen"] = True
 
-SkriptWerte = {}
+
 
 def testfunk():
     global EntladeFreigabeGesendet
+    global NetzLadenAusGesperrt
     
+    SkriptWerte = {}
     SkriptWerte["schaltschwelleNetzLadenaus"] = 10.0
     
     if BmsWerte["Akkuschutz"]:
@@ -59,24 +66,29 @@ def testfunk():
         SkriptWerte["schaltschwellePvNetz"] = 30.0
         SkriptWerte["schaltschwelleNetz"] = 15.0
         
+    # Wir setzen den Error bei 100 prozent zurück. In der Hoffunng dass nicht immer 100 prozent vom BMS kommen dieses fängt aber bei einem Neustart bei 0 proz an.
+    if BmsWerte["AkkuProz"] >= 100.0:
+        BmsWerte["Error"] = False
+        
     if BmsWerte["BmsEntladeFreigabe"] == True and BmsWerte["Error"] == False:
         EntladeFreigabeGesendet = False
-        # Wenn der Akku 40% hat wird er wieder Tag und Nacht genutzt
+        # Wenn der Akku wieder über die schaltschwelleAkku ist dann wird er wieder Tag und Nacht genutzt
         if not BmsWerte["WrMode"] == VerbraucherAkku and BmsWerte["AkkuProz"] >= SkriptWerte["schaltschwelleAkku"]:
             schalteAlleWrAufAkku()
             BmsWerte["Akkuschutz"] = False
             sendeMqtt = True
             if beVerbose == True:
                 print("Schalte alle WR auf Akku")
-        # Wenn der Akku 30% hat geben wir den Akku wieder frei wenn PV verfügbar ist. PV (Tag), Netz (Nacht)
+        # Wenn der Akku über die schaltschwellePvNetz ist dann geben wir den Akku wieder frei wenn PV verfügbar ist. PV (Tag), Netz (Nacht)
         elif BmsWerte["WrMode"] == VerbraucherNetz and BmsWerte["AkkuProz"] >= SkriptWerte["schaltschwellePvNetz"]:
             # Hier wird explizit nur geschalten wenn der WR auf VerbraucherNetz steht damit der Zweig nur reagiert wenn der Akku leer war und voll wird 
             schalteAlleWrNetzLadenAus()
             schalteAlleWrVerbraucherPVundNetz()
+            NetzLadenAusGesperrt = False
             sendeMqtt = True
             if beVerbose == True:
                 print("Schalte alle WR Verbraucher PV und Netz")
-        # Wenn die Verbraucher auf PV (Tag) und Netz (Nacht) geschaltet wurden und der Akku wieder unter 25% fällt dann wird auf Netz geschaltet
+        # Wenn die Verbraucher auf PV (Tag) und Netz (Nacht) geschaltet wurden und der Akku wieder unter die schaltschwelleNetz fällt dann wird auf Netz geschaltet
         elif BmsWerte["WrMode"] == VerbraucherPVundNetz and BmsWerte["AkkuProz"] <= SkriptWerte["schaltschwelleNetz"]:
             schalteAlleWrAufNetzOhneNetzLaden()
             BmsWerte["Akkuschutz"] = True
@@ -84,15 +96,22 @@ def testfunk():
             if beVerbose == True:
                 print("Schalte alle WR Netz ohne laden")
         # Wenn das Netz Laden durch eine Unterspannungserkennung eingeschaltet wurde schalten wir es aus wenn der Akku wieder 10% hat
-        elif BmsWerte["WrNetzladen"] == True and BmsWerte["AkkuProz"] >= SkriptWerte["schaltschwelleNetzLadenaus"]:
+        elif BmsWerte["WrNetzladen"] == True and NetzLadenAusGesperrt == False and BmsWerte["AkkuProz"] > SkriptWerte["schaltschwelleNetzLadenaus"]:
             schalteAlleWrNetzLadenAus()
-            BmsWerte["WrNetzladen"] = False
             sendeMqtt = True
             if beVerbose == True:
                 print("Schalte alle WR Netz laden aus")
+        elif BmsWerte["WrNetzladen"] == False and BmsWerte["Akkuschutz"] == True and BmsWerte["AkkuProz"] < (SkriptWerte["schaltschwelleNetzLadenaus"] - 2):
+            schalteAlleWrNetzLadenEin()
+            sendeMqtt = True
+            if beVerbose == True:
+                print("Schalte alle WR Netz laden ein")
     elif EntladeFreigabeGesendet == False:
         EntladeFreigabeGesendet = True
         schalteAlleWrAufNetz()
+        # Falls der Akkustand zu hoch ist würde nach einer Abschaltung das Netzladen gleich wieder abgeschaltet werden das wollen wir verhindern
+        if BmsWerte["AkkuProz"] >= SkriptWerte["schaltschwelleNetzLadenaus"]:
+            NetzLadenAusGesperrt = True
         # wir setzen einen error weil das nicht plausibel ist und wir hin und her schalten sollte die freigabe wieder kommen
         if BmsWerte["AkkuProz"] >= SkriptWerte["schaltschwellePvNetz"]:
             Bmswerte["Error"] = True
@@ -105,8 +124,20 @@ def testfunk():
             
             
 def test():
+
+# die folgenden Wertegrenzen wurden verwendet
+#    if BmsWerte["Akkuschutz"]:
+#        SkriptWerte["schaltschwelleAkku"] = 50.0
+#        SkriptWerte["schaltschwellePvNetz"] = 40.0
+#        SkriptWerte["schaltschwelleNetz"] = 25.0
+#    else:
+#        SkriptWerte["schaltschwelleAkku"] = 40.0
+#        SkriptWerte["schaltschwellePvNetz"] = 30.0
+#        SkriptWerte["schaltschwelleNetz"] = 15.0
+#        
+        
     global BmsWerte
-    print("xxxxxxxxxxxxxxxxxxxxxxx Normaler Zyklus xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxx 5-100-5 Zyklus xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     BmsWerte["AkkuProz"] = 5
     testfunk()   
     testfunk() 
@@ -145,7 +176,7 @@ def test():
     testfunk()   
     testfunk()
 
-    print("xxxxxxxxxxxxxxxxxxxxxxx Normaler Zyklus nach unterspannung xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxx 5-100-5 Zyklus nach unterspannung xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
     BmsWerte["AkkuProz"] = 5
     testfunk()   
@@ -186,7 +217,7 @@ def test():
     testfunk()   
     testfunk()    
     
-    print("xxxxxxxxxxxxxxxxxxxxxxx Zyklus mit Entladung -> Akkuschutz ausloesen xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxx 5-31-5 Zyklus mit Entladung -> Akkuschutz ausloesen xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
     BmsWerte["AkkuProz"] = 5
     testfunk()   
@@ -213,7 +244,7 @@ def test():
     testfunk()   
     testfunk()
     
-    print("xxxxxxxxxxxxxxxxxxxxxxx Test auf hoeheren Akkubereich Akkuschutz aktiv xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxx 11-41-6-51-9 Zyklus. Test auf hoeheren Akkubereich Akkuschutz aktiv xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     
     if BmsWerte["Akkuschutz"]:
         print("vvv Akkuschutz gesetzt")
@@ -250,6 +281,11 @@ def test():
     BmsWerte["AkkuProz"] = 9
     testfunk()   
     testfunk()
+    print("vvvvv jetzt muss es NetzLaden ein schalten")   
+    BmsWerte["AkkuProz"] = 6
+    testfunk()   
+    testfunk()    
+    print("vvvvv jetzt muss es NetzLaden aus schalten")   
     BmsWerte["AkkuProz"] = 27
     testfunk()   
     testfunk()
@@ -276,7 +312,7 @@ def test():
     testfunk()   
     testfunk()    
     
-    print("xxxxxxxxxxxxxxxxxxxxxxx Normaler Zyklus akkuschutz inaktiv xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+    print("xxxxxxxxxxxxxxxxxxxxxxx 11-41-5 Normaler Zyklus akkuschutz inaktiv xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
     if BmsWerte["Akkuschutz"]:
         print("vvv Akkuschutz gesetzt")
@@ -312,12 +348,66 @@ def test():
     BmsWerte["AkkuProz"] = 11
     testfunk()
     testfunk() 
-    BmsWerte["AkkuProz"] = 9
+    BmsWerte["AkkuProz"] = 5
     testfunk()   
     testfunk()    
     print("kein Print ist OK")    
+
+
+    print("xxxxxxxxxxxxxxxxxxxxxxx 17-41-5 Zyklus unterspannung und falschen soc xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     
+
+    BmsWerte["AkkuProz"] = 17
+    testfunk()   
+    testfunk() 
+
+    print("vvvvv jetzt muss es auf Netz mit Laden schalten") 
+    BmsWerte["BmsEntladeFreigabe"] = False
+    testfunk()   
+    testfunk()
+    BmsWerte["BmsEntladeFreigabe"] = True
+    testfunk()   
+    testfunk()
+
+    global NetzLadenAusGesperrt
     
+    if NetzLadenAusGesperrt:
+        print("vvv NetzLadenAusGesperrt gesetzt")
+        print("Ok")
+    else:
+        print("vvv NetzLadenAusGesperrt nicht gesetzt") 
+        print("error")
+        
+    if BmsWerte["WrNetzladen"]:
+        print("vvv WrNetzladen gesetzt") 
+        print("Ok")
+    else:
+        print("vvv WrNetzladen nicht gesetzt")
+        print("error")
+
+    BmsWerte["AkkuProz"] = 27
+    testfunk()    
+    testfunk()
+    print("vvvvv jetzt muss es auf PV und Netz schalten")
+    BmsWerte["AkkuProz"] = 31
+    testfunk()   
+    testfunk()
+    print("vvvvv jetzt muss es auf Akku schalten")    
+    BmsWerte["AkkuProz"] = 41
+    testfunk()   
+    testfunk() 
+    BmsWerte["AkkuProz"] = 31
+    testfunk()   
+    testfunk()
+    BmsWerte["AkkuProz"] = 27
+    testfunk()   
+    testfunk()
+    BmsWerte["AkkuProz"] = 11
+    testfunk()   
+    testfunk()
+    BmsWerte["AkkuProz"] = 5    
+    testfunk()   
+    testfunk()
     
 test()
 
